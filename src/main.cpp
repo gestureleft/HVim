c2804cb1d0bc63ca09d990326a8507261fef1bcd
gestureleft <harry130899@hotmail.com>
2020-08-28 10:50:23 +1000

    Added initial code for entering raw mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+#include <unistd.h>
+#include <termios.h>
+
+termios orig_termios;
+
+void disableRawMode()
+{
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) exit(errno);
+}
+
+void enableRawMode()
+{
+    std::atexit(disableRawMode);
+
+    termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) exit(errno);
+}
 
 int main()
 {
-    std::cout << "Hello from HVim" << '\n';
+    enableRawMode();
+    while (1)
+    {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) exit(errno);
+        std::cout << c << '\n';
+    }
     return 0;
 }
