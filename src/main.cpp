1732f9df8a40496362e54e87aac69ff1801f90a9
harrison-marshall-ws <harry130899@hotmail.com>
2020-08-29 13:40:48 +1000

    Added fallback method for getting window size for systems that don't support ioctl

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <array>
 
 #include <unistd.h>
 #include <termios.h>
@@ -28,18 +29,6 @@ void enable_raw_mode(const termios& orig_termios)
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) exit(errno);
 }
 
-std::pair<int,int> get_window_size()
-{
-    winsize ws;
-
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
-    {
-        throw 1;
-    } else {
-        return {ws.ws_row, ws.ws_col};
-    }
-}
-
 char editor_read_key()
 {
    std::size_t num_read;
@@ -51,19 +40,59 @@ char editor_read_key()
    return c;
 }
 
-void editor_draw_rows()
+std::pair<int,int> get_cursor_position()
 {
-    for (int y = 0; y < get_window_size().first; y++)
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) throw std::ios_base::failure(""); // Request cursor position
+    // The reply is in the form: \x1b[n;mR, where n is the row and m is the column
+    // The following code gets the cursor position out of this responses.
+
+    std::array<char, 32> in_chars{};
+
+    for (auto& c : in_chars)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        if (read(STDIN_FILENO, &c, 1) != 1) break;
+        if (c == 'R') break;
     }
+
+    if (in_chars.at(0) != '\x1b' || in_chars.at(1) != '[') throw std::ios_base::failure("");
+    std::pair<int,int> cursor_pos{};
+    if (std::sscanf(&in_chars.at(2), "%d;%d", &cursor_pos.first, &cursor_pos.second) != 2) throw std::ios_base::failure("");
+
+    return cursor_pos;
+}
+
+std::pair<int,int> get_window_size()
+{
+    winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    { // ioctl doesn't work on some systems. For those systems, move the cursor to the bottom right of screen and
+      // get cursor position.
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) throw std::ios_base::failure("");
+        return get_cursor_position();
+    } else {
+        return {ws.ws_row, ws.ws_col};
+    }
+}
+
+void editor_draw_rows(const size_t& height)
+{
+    std::string y_str{};
+    for (int y = 0; y < height - 1; y++)
+    {
+        y_str = std::to_string(y);
+        write(STDOUT_FILENO, y_str.c_str(), sizeof(y));
+        write(STDOUT_FILENO, "\r\n", 2);
+    }
+    write(STDOUT_FILENO, "xxx\r\n", 5);
 }
 
 void editor_refresh_screen()
 {
+    std::pair<int,int> window_size = get_window_size();
     write(STDOUT_FILENO, "\x1b[2J", 4); // Clear Screen
     write(STDOUT_FILENO, "\x1b[H", 3);  // Reset Cursor to top left
-    editor_draw_rows();
+    editor_draw_rows(window_size.first);
     write(STDOUT_FILENO, "\x1b[1;2H", 6);
 }
 
