b53fa2b2089184ec823a56e9628d290030c4e713
harrison-marshall-ws <harry130899@hotmail.com>
2020-09-21 15:15:26 +1000

    Wrote tests for Moving Forward and Back

--- hvim_tests/MoveCursorTest.cpp
+++ hvim_tests/MoveCursorTest.cpp
@@ -7,6 +7,19 @@
 #include "gtest/gtest.h"
 #include "EditorConfig.h"
 
+TEST(MoveCursorTest, MoveRight)
+{
+EditorConfig e({2,0}, 0, 0, {"Hello", "World"}, true);
+EXPECT_EQ(e.m_content.at(0), "Hello");
+EXPECT_EQ(e.m_cursor.x, 2);
+EXPECT_EQ(e.m_cursor.y, 0);
+EditorConfig e2(move_cursor(e, RIGHT), e.m_view_offset_x, e.m_view_offset_y, e.m_content, e.m_do_run);
+EXPECT_EQ(e2.m_cursor.x, 3);
+EXPECT_EQ(e2.m_cursor.y, 0);
+EXPECT_EQ(e.m_cursor.x, 2);
+EXPECT_EQ(e.m_cursor.y, 0);
+}
+
 TEST(MoveCursorTest, MoveForward)
 {
     EditorConfig e({0,0}, 0, 0, {"Hello", "World"}, true);
@@ -31,4 +44,30 @@ TEST(MoveCursorTest, MoveForwardEndOfLine)
     EXPECT_EQ(e2.m_cursor.y, 1);
     EXPECT_EQ(e.m_cursor.x, 4);
     EXPECT_EQ(e.m_cursor.y, 0);
+}
+
+TEST(MoveCursorTest, MoveBack)
+{
+    EditorConfig e({1,0}, 0, 0, {"Hello", "World"}, true);
+    EXPECT_EQ(e.m_content.at(0), "Hello");
+    EXPECT_EQ(e.m_cursor.x, 1);
+    EXPECT_EQ(e.m_cursor.y, 0);
+    EditorConfig e2(move_cursor(e, BACK), e.m_view_offset_x, e.m_view_offset_y, e.m_content, e.m_do_run);
+    EXPECT_EQ(e2.m_cursor.x, 0);
+    EXPECT_EQ(e2.m_cursor.y, 0);
+    EXPECT_EQ(e.m_cursor.x, 1);
+    EXPECT_EQ(e.m_cursor.y, 0);
+}
+
+TEST(MoveCursorTest, MoveBackAtStartOfLine)
+{
+    EditorConfig e({0,1}, 0, 0, {"Hello", "World"}, true);
+    EXPECT_EQ(e.m_content.at(0), "Hello");
+    EXPECT_EQ(e.m_cursor.x, 0);
+    EXPECT_EQ(e.m_cursor.y, 1);
+    EditorConfig e2(move_cursor(e, BACK), e.m_view_offset_x, e.m_view_offset_y, e.m_content, e.m_do_run);
+    EXPECT_EQ(e2.m_cursor.x, e2.m_content.at(e2.m_cursor.y).length() - 1);
+    EXPECT_EQ(e2.m_cursor.y, 0);
+    EXPECT_EQ(e.m_cursor.x, 0);
+    EXPECT_EQ(e.m_cursor.y, 1);
 }
